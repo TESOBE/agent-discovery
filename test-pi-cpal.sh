1d44958ecc00a0afcce2088384e3c2bcbf5d83af
simonredfern <simon@tesobe.com>
2026-02-21 04:33:08 +0100

    Update test-pi-cpal.sh

--- test-pi-cpal.sh
+++ test-pi-cpal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Show what cpal sees on this device.
+# Show what cpal sees on this device, saved to a file.
 # Run: bash test-pi-cpal.sh
 
-cargo run -- list-devices 2>&1
+cargo run -- list-devices > cpal-devices.txt 2>&1
+echo "Saved to cpal-devices.txt"
+head -20 cpal-devices.txt
